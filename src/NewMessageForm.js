0e09ac6bacf907942b8e864190adc6014ff7e035
Josh Justice <jjustice@bignerdranch.com>
2017-12-31 05:43:10 -0500

    Clear input upon clicking save
    Typically we'd create a component test for NewMessageForm to specify the behavior that the text input should be cleared out upon clicking Save. However, asserting against HTML properties isn't typical in Enzyme.
    
    Instead, we fix the behavior directly to pass this test.
    
    Red: Expected to find content: 'New message' but never did.

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 
 export default class NewMessageForm extends Component {
+  state = { inputText: '' }
+
+  handleTextChange = (event) => {
+    this.setState({ inputText: event.target.value });
+  }
+
+  handleSave = () => {
+    this.setState({ inputText: '' });
+  }
+
   render() {
+    const { inputText } = this.state;
     return (
       <div>
         <input
           type="text"
           data-test="messageText"
+          value={inputText}
+          onChange={this.handleTextChange}
         />
         <button
           data-test="saveButton"
+          onClick={this.handleSave}
         >
           Save
         </button>
