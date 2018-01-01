b6ac2a8b02d607605f6312a53b40b1dd0dcefa1b
Josh Justice <jjustice@bignerdranch.com>
2018-01-01 06:40:01 -0500

    Customize ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "extends": "airbnb"
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "plugins": [
+    "cypress",
+  ],
+  "env": {
+    "cypress/globals": true,
+    "jest": true,
+  },
+  "rules": {
+    "no-use-before-define": 0,
+    "react/jsx-filename-extension": 0,
+    "react/prop-types": 0,
+  }
 };
