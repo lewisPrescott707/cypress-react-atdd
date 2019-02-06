1dd7aa8fe8ffff84f37304ca83a054b92af5ed8b
Josh Justice <me@codingitwrong.com>
2019-02-06 08:15:00 -0500

    Make component controlled

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewMessageForm = () => {
+  const [inputText, setInputText] = useState('');
+
+  const handleTextChange = (event) => {
+    setInputText(event.target.value);
+  };
+
   return (
     <div>
       <input
         type="text"
         data-testid="messageText"
+        value={inputText}
+        onChange={handleTextChange}
       />
       <button
         data-testid="sendButton"
