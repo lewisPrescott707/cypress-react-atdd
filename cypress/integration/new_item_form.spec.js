72165fc4b4aebce1de253452aab7daee6e4ef870
Lewis Prescott <lewis.prescott@ceracare.co.uk>
2021-12-03 09:48:47 +0000

    failing cypress test

--- cypress/integration/new_item_form.spec.js
+++ cypress/integration/new_item_form.spec.js
@@ -1,5 +1,11 @@
 describe('New Item form', () => {
   it('should remove text from input when submitted', () => {
     cy.visit('http://localhost:3000');
+    
+    cy.get('[data-cy="messageText"]').type('New');
+
+    cy.get('[data-cy="submitBtn"]').click();
+
+    cy.get('[data-cy="messageText"]').should('have.value', '');
   });
 });
